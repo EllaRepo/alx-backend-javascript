93f155c2bd0df44ef5b525b2182c425f1aac4237
Eliab E <eliaberango@gmail.com>
2023-11-30 09:04:52 -0800

    Refactored

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,7 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  const employees = departments.reduce((acc, department) => [...acc, ...department], []);
-  let currentIndex = 0;
-
-  return {
-    next() {
-      if (currentIndex < employees.length) {
-        const value = employees[currentIndex];
-        currentIndex += 1;
-        return { value, done: false };
-      }
-      return { done: true };
-    },
-  };
+  const result = [];
+  for (const emp of Object.values(report.allEmployees)) {
+    result.push(...emp);
+  }
+  return result;
 }
