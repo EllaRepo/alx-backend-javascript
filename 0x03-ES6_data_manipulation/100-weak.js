5c1a2d48838449e7b933d07dcfae964accebe4b3
Eliab E <eliaberango@gmail.com>
2023-12-05 12:56:58 -0800

    Add 100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+
+export const queryAPI = (endpoint) => {
+  if (weakMap.has(endpoint)) {
+    const count = weakMap.get(endpoint);
+    if (count >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, (count + 1));
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+};
