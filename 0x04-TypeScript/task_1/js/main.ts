e032f728f038766d02115abb8c1ac1fc8c2aee78
Eliab E <eliaberango@gmail.com>
2023-12-06 01:49:00 -0800

    Add StudentClass class implementation

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -22,3 +22,30 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+// Define the Student interface
+interface Student {
+    firstName: string;
+    lastName: string;
+}
+
+// Define the StudentClass that implements the Student interface
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Method that returns the string "Currently working"
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    // Method that returns the firstName of the student
+    displayName(): string {
+        return this.firstName;
+    }
+}
