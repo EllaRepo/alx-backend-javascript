df322d2573dc0d15c98009ed1539a07c574865d0
Eliab E <eliaberango@gmail.com>
2024-01-29 23:59:00 -0800

    Fix style error and handle on exit message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,17 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', function(data) {
-  const input = data.trim();
-  process.stdout.write("Your name is: " + input + "\n");
-});
-
-process.on('exit', function() {
-  process.stdout.write("This important software is now closing\n");
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
